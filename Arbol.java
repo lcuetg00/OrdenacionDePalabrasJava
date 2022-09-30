bb53b65b4f870fd2a934712f59b435cfb9f9bcca
lcuetg00 <lcuetg00@estudiantes.unileon.es>
2022-09-30 13:10:00 +0200

    Actualizadas clases Main y Arbol
    Creada la estructura y funcionalidad de la clase arbol:
    -Forma de crear la estructura de arbol
    -Forma de reccorrerla

--- Arbol.java
+++ Arbol.java
@@ -2,5 +2,86 @@ import java.util.ArrayList;
 
 
 public class Arbol {
-
+	public final int primerCaracter='A';
+	public final int numeroCaracteres='Z'-'A'+1;
+	
+	Nodo raiz;
+	
+	public Arbol() {
+		raiz=new Nodo(null);
+	}
+	
+	/**
+	 * Inserta una palabra en el arbol.
+	 * Precondicion s solo debe de contener caracteres mayuscula y no debe tener
+	 * espacios.
+	 * @param s
+	 */
+	public void insertar(String s) {
+		Nodo siguienteNodo=raiz;
+		for(int i=0;i<s.length();i++) {
+			int posicion=s.charAt(i)-primerCaracter;
+			if(siguienteNodo.arrayNodos[posicion]==null) {
+				siguienteNodo.arrayNodos[posicion]=new Nodo(siguienteNodo);
+			}
+			siguienteNodo=siguienteNodo.arrayNodos[posicion];
+		}
+		siguienteNodo.terminaPalabra=true;
+	}
+	
+	public void insertarListaPalabras(String[] lista) {
+		for(int i=0;i<lista.length;i++) {
+			insertar(lista[i].toUpperCase());
+		}
+	}
+	
+	/**
+	 * Devuelve una lista de palabras ordenadas a partir de palabras previamente insertadas.
+	 * @return
+	 */
+	public ArrayList<String> recogerPalabras() {
+		ArrayList <String> cadenas=new ArrayList<>();
+		ArrayList <Character> letras=new ArrayList<>();
+		Nodo siguienteNodo=raiz;
+		int i=0;
+		
+		do {
+			for(;i<numeroCaracteres;i++) {
+				if(siguienteNodo.arrayNodos[i]!=null) {
+					char letra= (char)(primerCaracter+i);
+					letras.add(letra);
+					siguienteNodo=siguienteNodo.arrayNodos[i];
+					i=-1; //reinicio el puntero para empezar en A del siguiente caracter
+					if(siguienteNodo.terminaPalabra==true) {
+						char[] c = new char[letras.size()];
+						for (int j=0; j<letras.size(); j++) {
+							c[j] = letras.get(j).charValue();
+						}
+						cadenas.add(new String(c));
+					}
+				}
+			}
+			if (siguienteNodo!=raiz) {
+			siguienteNodo=siguienteNodo.nodoPadre;
+			i=letras.get(letras.size()-1)-primerCaracter+1;
+			letras.remove(letras.size()-1);
+			} else {
+				break;
+			}
+			
+		} while(true);
+		
+		return cadenas;
+	}
+	
+	public class Nodo {
+		Nodo[] arrayNodos;
+		Nodo nodoPadre;
+		boolean terminaPalabra=false;
+		
+		protected Nodo(Nodo nodoPadre) {
+			arrayNodos= new Nodo[numeroCaracteres];
+			this.nodoPadre=nodoPadre;
+		}
+	}
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String[] lista = new String[] {"hola","mundo","mango",};
+		
+		Arbol arbol = new Arbol();
+		arbol.insertarListaPalabras(lista);
+		
+		ArrayList<String> listaOrdenada = arbol.recogerPalabras();
+		
+		for(int i=0;i<listaOrdenada.size();i++) {
+			System.out.println(listaOrdenada.get(i));
+		}
 		
 	}
 }
